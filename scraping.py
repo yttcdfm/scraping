af802ba855836cdb7d13dd03652ef5a5e1080442
yttcdfm <ytt.akbakb84@gmail.com>
2020-09-26 16:34:30 +0900

    UTF-8のつづりが違うので直した

--- scraping.py
+++ scraping.py
@@ -316,7 +316,7 @@ def scraping(fileobj, site_title, url):
 ##############################  
 dt_now = datetime.datetime.now()
 file = dt_now.strftime('%Y%m%d_%H%M')+".sql"
-fileobj = open(file, "w", encoding = "utf_8")
+fileobj = open(file, "w", encoding = "utf-8")
 
 #scraping(fileobj, SiteName.TEST, 'https://javmix.tv/video/')
 scraping(fileobj, SiteName.NUKISUTO, 'https://www.nukistream.com/')
